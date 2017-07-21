8e34856373e82aedf21ae364fa5d52051f174ea6
cescoferraro <francescoaferraro@gmail.com>
2017-07-21 10:44:34 -0300

    prettier.js and using cescoferraro/favicons

--- index.js
+++ index.js
@@ -4,52 +4,65 @@ var assert = require('assert');
 var fs = require('fs');
 var path = require('path');
 
-function FaviconsWebpackPlugin (options) {
+function FaviconsWebpackPlugin(options) {
   if (typeof options === 'string') {
-    options = {logo: options};
+    options = { logo: options };
   }
-  assert(typeof options === 'object', 'FaviconsWebpackPlugin options are required');
+  assert(
+    typeof options === 'object',
+    'FaviconsWebpackPlugin options are required'
+  );
   assert(options.logo, 'An input file is required');
-  this.options = Object.assign({
-    prefix: 'icons-[hash]/',
-    emitStats: false,
-    statsFilename: 'iconstats-[hash].json',
-    persistentCache: true,
-    inject: true
-  }, options);
+  this.options = Object.assign(
+    {
+      prefix: 'icons-[hash]/',
+      emitStats: false,
+      statsFilename: 'iconstats-[hash].json',
+      persistentCache: true,
+      inject: true
+    },
+    options
+  );
 
-  this.options.config = Object.assign({
-    appleStatusBarStyle: 'black-translucent',
-    background: '#fff',
-    theme_color: '#fff',
-    gcm_sender_id: '',
-  }, options.config);
+  this.options.config = Object.assign(
+    {
+      appleStatusBarStyle: 'black-translucent',
+      background: '#fff',
+      theme_color: '#fff',
+      gcm_sender_id: ''
+    },
+    options.config
+  );
 
-  this.options.config.icons = Object.assign({
-    android: true,
-    appleIcon: true,
-    appleStartup: true,
-    coast: false,
-    favicons: true,
-    firefox: true,
-    opengraph: false,
-    twitter: false,
-    yandex: false,
-    windows: false
-  }, this.options.config.icons);
+  this.options.config.icons = Object.assign(
+    {
+      android: true,
+      appleIcon: true,
+      appleStartup: true,
+      coast: false,
+      favicons: true,
+      firefox: true,
+      opengraph: false,
+      twitter: false,
+      yandex: false,
+      windows: false
+    },
+    this.options.config.icons
+  );
 }
 
-FaviconsWebpackPlugin.prototype.apply = function (compiler) {
+FaviconsWebpackPlugin.prototype.apply = function(compiler) {
   var self = this;
   if (!self.options.config.appName) {
     self.options.config.appName = guessAppName(compiler.context);
-  } 
+  }
 
   // Generate the favicons
   var compilationResult;
-  compiler.plugin('make', function (compilation, callback) {
-    childCompiler.compileTemplate(self.options, compiler.context, compilation)
-      .then(function (result) {
+  compiler.plugin('make', function(compilation, callback) {
+    childCompiler
+      .compileTemplate(self.options, compiler.context, compilation)
+      .then(function(result) {
         compilationResult = result;
         callback();
       })
@@ -59,11 +72,16 @@ FaviconsWebpackPlugin.prototype.apply = function (compiler) {
   // Hook into the html-webpack-plugin processing
   // and add the html
   if (self.options.inject) {
-    compiler.plugin('compilation', function (compilation) {
-      compilation.plugin('html-webpack-plugin-before-html-processing', function (htmlPluginData, callback) {
+    compiler.plugin('compilation', function(compilation) {
+      compilation.plugin('html-webpack-plugin-before-html-processing', function(
+        htmlPluginData,
+        callback
+      ) {
         if (htmlPluginData.plugin.options.favicons !== false) {
           htmlPluginData.html = htmlPluginData.html.replace(
-            /(<\/head>)/i, compilationResult.stats.html.join('') + '$&');
+            /(<\/head>)/i,
+            compilationResult.stats.html.join('') + '$&'
+          );
         }
         callback(null, htmlPluginData);
       });
@@ -72,7 +90,7 @@ FaviconsWebpackPlugin.prototype.apply = function (compiler) {
 
   // Remove the stats from the output if they are not required
   if (!self.options.emitStats) {
-    compiler.plugin('emit', function (compilation, callback) {
+    compiler.plugin('emit', function(compilation, callback) {
       delete compilation.assets[compilationResult.outputName];
       callback();
     });
@@ -82,7 +100,7 @@ FaviconsWebpackPlugin.prototype.apply = function (compiler) {
 /**
  * Tries to guess the name from the package.json
  */
-function guessAppName (compilerWorkingDirectory) {
+function guessAppName(compilerWorkingDirectory) {
   var packageJson = path.resolve(compilerWorkingDirectory, 'package.json');
   if (!fs.existsSync(packageJson)) {
     packageJson = path.resolve(compilerWorkingDirectory, '../package.json');
